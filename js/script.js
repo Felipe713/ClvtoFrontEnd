25dea62b5e3ac9b2fd31ea5fa250becffc874695
Felipe Valenzuela <74212825+Felipe713@users.noreply.github.com>
2024-03-13 19:17:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,48 @@
 // script.js
 
 document.addEventListener('DOMContentLoaded', function () {
-    var contactForm = document.getElementById('contactForm');
+    var contactForm = document.querySelector('form');
 
     contactForm.addEventListener('submit', function (event) {
-        // Validar el nombre
-        var nombreInput = document.getElementById('nombre');
-        var nombreRegex = /^[a-zA-Z\s]+$/;
+        var nombreInput = document.getElementById('inputName4');
+        var emailInput = document.getElementById('inputEmail4');
+        var telefonoInput = document.getElementById('inputNumber4');
+        var mensajeInput = document.getElementById('inputMessage');
 
-        if (!nombreRegex.test(nombreInput.value)) {
+        if (!validateNombre(nombreInput.value)) {
             alert('Por favor, ingrese un nombre válido sin números ni caracteres especiales.');
             event.preventDefault();
             return;
         }
 
-        // Validar el correo electrónico
-        var emailInput = document.getElementById('email');
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-        if (!emailRegex.test(emailInput.value)) {
+        if (!validateEmail(emailInput.value)) {
             alert('Por favor, ingrese una dirección de correo electrónico válida.');
             event.preventDefault();
             return;
         }
+
+        if (!validateTelefono(telefonoInput.value)) {
+            alert('Por favor, ingrese un número de teléfono válido.');
+            event.preventDefault();
+            return;
+        }
+
+        if (mensajeInput.value.trim() === '') {
+            alert('Por favor, ingrese un mensaje.');
+            event.preventDefault();
+            return;
+        }
     });
+
+    function validateNombre(nombre) {
+        return /^[a-zA-Z\s]+$/.test(nombre);
+    }
+
+    function validateEmail(email) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    }
+
+    function validateTelefono(telefono) {
+        return /^\d{9}$/.test(telefono);
+    }
 });
